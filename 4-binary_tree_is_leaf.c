0dfdb048a799296f76da6c709ed72ed59d02d969
bashar-karoury <basharalkaroury@hotmail.com>
2024-02-29 15:10:23 +0400

    task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,9 +10,9 @@
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (tree)
+	if (node)
 	{
-		if ((tree->left) && (tree->right))
+		if ((node->left == NULL) && (node->right == NULL))
 			return (1);
 	}
 	return (0);
